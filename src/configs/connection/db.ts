ba36136aa916e74f77724f0a6261a7d32c7cf37e
Ng Han Yi <nghanyi1997@gmail.com>
2022-04-26 15:55:25 +0800

    add mssql connections :sparkles:

--- src/configs/connection/db.ts
+++ src/configs/connection/db.ts
@@ -3,13 +3,57 @@ import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize/types';
 
 const modelsPath = path.join(__dirname, '../../models');
-const db = new Sequelize({
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const configSqlite = {
     database: String(process.env.DB_NAME),
     username: String(process.env.DB_USER),
     password: String(process.env.DB_PASSWORD),
-    dialect: <Dialect>process.env.DB_DRIVER,
+    dialect: <Dialect>'sqlite',
     storage: 'makan-express.sqlite',
     models: [modelsPath],
-});
+};
 
-export default db;
+const sqliteDb = new Sequelize(configSqlite);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const configMssql = {
+    host: String(process.env.DB_HOST),
+    dialect: <Dialect>'mssql',
+    port: 1433,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 5000,
+    },
+    retry: {
+        max: 20,
+    },
+    models: [modelsPath],
+};
+
+/**
+ * NOTE: Uncomment the following lines to use mssql.
+ */
+
+/*const mssqlDb = new Sequelize(
+    String(process.env.DB_USER),
+    String(process.env.DB_NAME),
+    String(process.env.DB_PASSWORD),
+    configMssql,
+);
+
+(async () => {
+    try {
+        await mssqlDb.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();*/
+
+export {
+    sqliteDb,
+    // mssqlDb,
+};
+export default sqliteDb;
