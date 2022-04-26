f07328a888e9ac74c07fea0b9cfeb8c96383b6fe
Han Yi <nghanyi1997@gmail.com>
2022-04-27 00:26:04 +0800

    use api response :sparkles:

--- src/configs/middlware/error.ts
+++ src/configs/middlware/error.ts
@@ -1,8 +1,12 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import HttpException from '../../utils/http_exception';
+import * as apiResponse from '../../controllers/api_response';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-    const status = error.statusCode || error.status || 500;
+    const status = error.statusCode || error.status || StatusCodes.INTERNAL_SERVER_ERROR;
 
-    res.status(status).send(error);
+    const response = apiResponse.error(error.message, status);
+    res.status(response.code).send(response.response);
 };
--- src/configs/middlware/not_found.ts
+++ src/configs/middlware/not_found.ts
@@ -1,7 +1,9 @@
-import { Request, Response, NextFunction, response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as apiResponse from '../../controllers/api_response';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
-    const message = 'Resource not found';
-
-    res.status(404).send(message);
+    const response = apiResponse.error('Resource not found', StatusCodes.NOT_FOUND);
+    res.status(response.code).send(response.response);
 };
