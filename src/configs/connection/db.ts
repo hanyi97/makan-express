2f08ba68a0b6c3afaf36fb36aa6275bafbbd2723
Ng Han Yi <nghanyi1997@gmail.com>
2022-04-26 12:47:51 +0800

    read dialect from .env :wrench:

--- src/configs/connection/db.ts
+++ src/configs/connection/db.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 import { Sequelize } from 'sequelize-typescript';
+import { Dialect } from 'sequelize/types';
 
 const modelsPath = path.join(__dirname, '../../models');
 const db = new Sequelize({
     database: String(process.env.DB_NAME),
     username: String(process.env.DB_USER),
     password: String(process.env.DB_PASSWORD),
-    dialect: 'sqlite',
+    dialect: <Dialect>process.env.DB_DRIVER,
     storage: 'makan-express.sqlite',
     models: [modelsPath],
 });
